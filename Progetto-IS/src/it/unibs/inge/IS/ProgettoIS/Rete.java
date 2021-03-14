a832711a064dc046f60605c5ef24cc16da07840b
Edoardo Manenti <edoardomanenti@hotmail.com>
2021-03-14 23:13:18 +0100

    Avanzamento su tutte le classi

--- Progetto-IS/src/it/unibs/inge/IS/ProgettoIS/Rete.java
+++ Progetto-IS/src/it/unibs/inge/IS/ProgettoIS/Rete.java
@@ -1,5 +1,6 @@
 package it.unibs.inge.IS.ProgettoIS;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Rete {
@@ -7,33 +8,57 @@ public class Rete {
 	 * Classe di modellazione della Rete, contiene tutte le informazioni riguardanti i nodi e le transizioni
 	 */
 	private HashMap<String, Nodo> nodi;
+	private ArrayList<Arco> archi;
 	private IORete managerIO;
 	
 	public Rete() {
 		this.nodi = new HashMap<String, Nodo>();
+		this.archi = new ArrayList<Arco>();
 		this.managerIO = new IORete();
 	}
 	
+	public boolean containsNodo(String n) {
+		return nodi.containsKey(n);
+	}
+
 	public boolean containsNodo(Nodo n) {
 		return nodi.containsValue(n);
 	}
 	
-	public boolean aggiungiTransizione(Posto p, Transizione t) {
-		if(!nodi.containsValue(p)) {
+	public void aggiungiArco(Arco arco) {
+		Nodo o = arco.getOrigin();
+		Nodo d = arco.getDestination();
+
+		if(!(nodi.containsValue(o))) 
+		{ 
+			nodi.put(o.getId(), o);
+		}
+
+		if(!(nodi.containsValue(d))) {
+			nodi.put(d.getId(), d);
+		}
+		
+		this.archi.add(arco); 
+		if(o.isPosto()) { 
+			Posto p = (Posto)arco.getOrigin();
+			Transizione t = (Transizione)arco.getDestination();
 			p.aggiungiSucc(t);
 			t.aggiungiPrec(p);
-			return true;
 		}
-		return false;
+		else {
+			Transizione t = (Transizione)arco.getOrigin(); 
+			Posto p = (Posto)arco.getDestination();
+			t.aggiungiSucc(p); 
+			p.aggiungiPrec(t);
+		}
 	}
 	
-	public boolean aggiungiPosto(Transizione t, Posto p) {
-		if(!nodi.containsValue(t)) {
-			t.aggiungiSucc(p);
-			p.aggiungiPrec(t);
-			return true;
-		}
-		return false;
+	public void aggiungiPosto(Posto p) {
+		this.nodi.put(p.getId(), p);
+	}
+
+	public void aggiungiTransizione(Transizione t) {
+		this.nodi.put(t.getId(), t);
 	}
 	
 	/*
@@ -41,6 +66,10 @@ public class Rete {
 	 */
 	public Nodo getNodo(String nomeNodo) {
 		return nodi.get(nomeNodo);
+	}
+	
+	public HashMap<String, Nodo> getNodi(){
+		return this.nodi;
 	}
 
 	// TO-DO list :
