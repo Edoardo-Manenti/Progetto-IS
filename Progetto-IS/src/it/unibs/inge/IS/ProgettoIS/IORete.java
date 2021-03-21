2afc8c5972d6d706020a70b43798ccdc762953b1
edoar <edoar@DESKTOP-2P44KE4.station>
2021-03-21 16:37:54 +0100

    Disponibili ora metodi getRetiSalvate, numeroRetiSalvate e rinominaRete

--- Progetto-IS/src/it/unibs/inge/IS/ProgettoIS/IORete.java
+++ Progetto-IS/src/it/unibs/inge/IS/ProgettoIS/IORete.java
@@ -3,6 +3,9 @@ package it.unibs.inge.IS.ProgettoIS;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -13,13 +16,15 @@ import java.util.Scanner;
 public class IORete {
 	private JsonUtils jsonUtil;
 	private String wdPath = "Reti";
+	private File wd;
 	private String separatore; 
+	private HashMap<String,File> retiSalvate;
 	
-	// Il path di default è la directory da cui è stato lanciato il programma
 	public IORete() {
 		this.jsonUtil = new JsonUtils();
+		this.retiSalvate = caricaRetiSalvate();
 		this.separatore = File.separator;
-		File wd = new File(wdPath);
+		this.wd = new File(wdPath);
 		if(!wd.exists()) {
 			wd.mkdir();
 		}
@@ -29,12 +34,31 @@ public class IORete {
 		this.wdPath = path;
 	}
 	
+	private HashMap<String, File> caricaRetiSalvate() {
+		HashMap<String, File> retiSalvate = new HashMap<String, File>();
+		for(File f : wd.listFiles()) {
+			retiSalvate.put(f.getName(), f);
+		}
+		return retiSalvate;
+	}
+	
+	public List<String> getNomiRetiSalvate() {
+		return new ArrayList<String>(retiSalvate.keySet());
+	}
+	
+	public int numeroRetiSalvate() {
+		return retiSalvate.size();
+	}
+	
 	public void salvaRete(Rete rete, String nomeRete) {
 		String json = jsonUtil.compilaJson(rete);
 		File file = new File(wdPath+separatore+nomeRete);
 		try {
 			if(!file.exists()) {
 				file.createNewFile();
+			}
+			else {
+				file = retiSalvate.get(nomeRete);
 			}
 		}
 		catch(IOException ex) {
@@ -44,15 +68,17 @@ public class IORete {
 		try ( FileWriter writer = new FileWriter(file))
 		{
 			writer.write(json);
+			retiSalvate.put(nomeRete, file);
 		}
 		catch(Exception ex) {
 			System.out.println(ex.getMessage());
 		}
 	}
+
 	
 	public Rete caricaRete(String reteRichiesta) {
 		Rete rete = new Rete();
-		try ( Scanner scanner = new Scanner(new File(wdPath+separatore+reteRichiesta)) 
+		try ( Scanner scanner = new Scanner(retiSalvate.get(reteRichiesta)) 
 				)
 		{
 			String json = "";
@@ -66,5 +92,15 @@ public class IORete {
 			System.out.println(ex.getMessage());
 		}
 		return rete;
+	}
+	
+	public boolean rinominaRete(String nomeRete, String nuovoNome) {
+		if(!retiSalvate.containsKey(nomeRete)) {
+			return false;
+		}
+		else {
+			File rete = retiSalvate.get(nomeRete);
+			return rete.renameTo(new File(nuovoNome));
+		}
 	}
 }
