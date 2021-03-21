d439d37949664a35bf27a2e44a3c223e6633bc8a
Edoardo Manenti <edoardomanenti@hotmail.com>
2021-03-22 00:15:19 +0100

    Aggiungi commenti sui due possibili modelli per la creazione della rete

--- Progetto-IS/src/it/unibs/inge/IS/ProgettoIS/ManagerRete.java
+++ Progetto-IS/src/it/unibs/inge/IS/ProgettoIS/ManagerRete.java
@@ -1,6 +1,5 @@
 package it.unibs.inge.IS.ProgettoIS;
 
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class ManagerRete {
@@ -30,6 +29,59 @@ public class ManagerRete {
         Rete nuovaRete = new Rete();
         String nuovoArco = InputDati.leggiStringaNonVuota(MESSAGGIO_CREAZIONE_ARCO);
     }
+    
+    /*
+     * MODELLO 1:
+     * 
+     * 1) Scelta tra nuova rete o visualizza rete
+     * 2.1) se nuova rete:
+     * 2.1.1) Crea posto iniziale o transizione iniziale
+     * 2.1.2) Scelta tra arco T->P o arco P->T o termina modifiche
+     * 2.1.3.1.1) se T->P:
+     * 2.1.3.1.2) Indica nomeT e nomeP
+     * 2.1.3.1.3) Controllo che almeno uno dei due esista
+     * 2.1.3.1.4) se esiste -> creo nodo mancante e aggiungo arco
+     * 2.1.3.1.5) se esistono già entrambi e l'arco non esiste, lo aggiungo, altrimenti informo che l'arco già esiste
+     * 2.1.3.1.6) Torno a 2.1.2
+     * 2.1.3.2.1) se P->T: analogo a T->P
+     * 2.1.3.3.1) se termina modifiche:
+     * 2.1.3.3.2) scelta tra salvare la rete o scartare le modifiche
+     * 2.1.3.3.3) se salva rete : scelgi nome con cui salvare la rete
+     * 2.2.1) se visualizza rete: presenta i nomi e chiede di scegliere la rete o torna menu principale
+     * 2.2.2) visualizza rete scelta 
+     * 
+     * 
+     * MODELLO 2:
+     * 
+     * A ogni passo chiedo se si vuole aggiungere/eliminare un nuovo posto, 
+     *        aggiungere/eliminare una transizione, aggiungere/eliminare un arco, terminare le modifiche
+     * se nuovo posto:
+     * - chiedo id
+     * - controllo che non esista
+     * 
+     * se nuova transizione:
+     * - chiedo id
+     * - controllo che non esista
+     * 
+     * se nuovo arco:
+     * - chiedo origine e destinazione
+     * - controllo che esistano ENTRAMBI
+     * - controllo che siano di diverso tipo
+     * - controllo che l'arco non sia già presente
+     * - aggiungo arco
+     * 
+     * se elimina arco/transizione -> chiedo id ed elimino
+     * 
+     * se termina modifiche:
+     * - controllo che ogni transizione abbia almeno un precedente E almeno un successivo
+     * - controllo che ogni posto abbia almeno un precedento O un successivo
+     * - altrimenti informo del Nodo che non rispetta tale condizione e ritorno allo stadio di modifica
+     * - salvo rete
+     * 
+     */
+    
+    
+    
     public void visualizzaRete(){
         int nrretiSalvate = ioRete.numeroRetiSalvate();
         if(nrretiSalvate == 0){
