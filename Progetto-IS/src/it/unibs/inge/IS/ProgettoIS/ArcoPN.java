08c7f20b6f2b5545f7df63025f5f4917011aec80
MassimilianoT <massimiliano.tummolo@ieee.org>
2021-03-25 15:13:22 +0100

    Inizio versione 2

---
+++ Progetto-IS/src/it/unibs/inge/IS/ProgettoIS/ArcoPN.java
@@ -0,0 +1,10 @@
+package it.unibs.inge.IS.ProgettoIS;
+
+import model.Arco;
+import model.Nodo;
+
+public class ArcoPN extends Arco {
+    public ArcoPN(Nodo o, Nodo d) {
+        super(o, d);
+    }
+}
---
+++ Progetto-IS/src/it/unibs/inge/IS/ProgettoIS/RetePN.java
@@ -0,0 +1,16 @@
+package it.unibs.inge.IS.ProgettoIS;
+
+import model.Rete;
+
+import java.util.ArrayList;
+
+public class RetePN extends Rete {
+    //INIZIO della Versione 2
+    // - Pesi agli archi -> creare classe ArcoPN
+    // - Classe Token -> avrà un'istanza dentro la classe NodoPN
+    //  - identificare marcatura corrente: usando i token contenuti nei nodi PN
+    // - Override dei metodi aggiungiarco etcetc
+    // - Metodo che genera in automatico da ReteN a RetePN
+    // - Nel ManagerRete aggiungere importazione reteN
+    // - Gestire il salvataggio delle retiPN
+}
