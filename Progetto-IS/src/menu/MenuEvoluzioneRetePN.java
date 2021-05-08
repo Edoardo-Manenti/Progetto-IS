1f57092db35f92b0b62f3eb899c3f91634556469
MassimilianoT <m.tummolo@studenti.unibs.it>
2021-05-08 14:24:19 +0200

    Aggiunte note sul metodo evoluzioneRetePN

--- Progetto-IS/src/menu/MenuEvoluzioneRetePN.java
+++ Progetto-IS/src/menu/MenuEvoluzioneRetePN.java
@@ -2,6 +2,7 @@ package menu;
 
 import model.Rete;
 import modelPN.RetePN;
+import utils.InputDati;
 
 public class MenuEvoluzioneRetePN {
     RetePN reteDaEvolvere;
@@ -18,9 +19,17 @@ public class MenuEvoluzioneRetePN {
         }
         while(reteDaEvolvere.hasTransizioniAbilitate()){
             //Qui dentro vuol dire che ho almeno una transizione abilitata faccio scegliere o no all'utente
-            //1. Se c'è solo una transizione lo dico e mostro l'evoluzione
+            //1. Se c'è solo una transizione la scatto e mostro la marcatura
             //2, Se ce ne sono di più applico il solito pattern per fare vedere all'utente una lista di alternative e operare una scelta
             //vedasi visualizzaRetePN nel MainMenu
+
+            //Dopo i due casi esclusivi 1. e 2. chiedo all'utente se vuole continuare
+
+            boolean continuare = InputDati.yesOrNo("Si desidera continuare nell'evoluzione? ");
+            if(!continuare) {
+                System.out.println("Evoluzione terminata.");
+                return; //termina subito il metodo
+            }
         }
         //Qui mostro all'utente che l'evoluzione è finita.
         System.out.println("Evoluzione terminata.");
